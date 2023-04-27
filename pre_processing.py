6ef6015b4d20acdedcd23a0756e88c2439db8242
sunaarun <sanaharun91@gmail.com>
2023-04-27 21:01:12 +0200

    Second Commit- Updates small things

--- pre_processing.py
+++ pre_processing.py
@@ -1,40 +1,51 @@
 import cv2
-
-import pytesseract
-pytesseract.pytesseract.tesseract_cmd = 'C:\Program Files\Tesseract-OCR'
+# import pytesseract
+# pytesseract.pytesseract.tesseract_cmd = 'C:\Program Files\Tesseract-OCR'
 
 def preProcessing(myImage):
     grayImg = cv2.cvtColor(myImage, cv2.COLOR_BGR2GRAY)
     ret, thresh1 = cv2.threshold(grayImg, 0, 255, cv2.THRESH_OTSU | cv2.THRESH_BINARY_INV)
-    print(ret)
+    print(f'The threshold valua applied to the image is: {ret} ')
     horizontal_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (18, 18))
     dilation = cv2.dilate(thresh1, horizontal_kernel, iterations=1)
     horizontal_contours, hierarchy = cv2.findContours(dilation, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
     im2 = myImage.copy()
     for cnt in horizontal_contours:
         x, y, w, h = cv2.boundingRect(cnt)
-        rect = cv2.rectangle(im2, (x, y), (x + w, y + h), (255, 255, 255), 2)
-    #im2= seg_word(rect)
-    #im2 = seg_word(im2)
-    im2=character_seg(im2)
+        rect = cv2.rectangle(im2, (x, y), (x + w, y + h), (255, 255, 255), 0)
+    im2= seg_word(rect)
+    #im2 = seg_word(rect)
+    #im2=character_seg(im2)
     return im2
 
 
 def seg_word(wordImage):
+    # conver the input image int gray scale
     grayImg = cv2.cvtColor(wordImage, cv2.COLOR_BGR2GRAY)
+    # Binarize the gray image with OTSU algorithm
     ret, thresh2 = cv2.threshold(grayImg, 0, 255, cv2.THRESH_OTSU | cv2.THRESH_BINARY_INV)
-    print(ret)
-    vertical_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (8, 17))
+    #print(ret)
+    # create a Structuring Element size of 8*10 for the vertical contouring
+    vertical_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (8, 10))
+    # apply Dilation for once only
     dilation = cv2.dilate(thresh2, vertical_kernel, iterations=1)
+    #fingd the vertical contours
     vertical_contours, hierarchy = cv2.findContours(dilation, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-    im2 = wordImage.copy()
+    word_img = wordImage.copy()
+    # Run through each contour and extract the bounding box
     for cnt in vertical_contours:
+        #computes the minimum rectangle
         x, y, w, h = cv2.boundingRect(cnt)
-        rect = cv2.rectangle(im2, (x, y), (x + w, y + h), (255, 0, 0), 2)
-    return rect
+        # Draw a rectangular from the top left to the bottom right with the
+        # given Coordinates x,y and height and width
+        rect = cv2.rectangle(word_img, (x, y), (x + w, y + h), (0, 255, 0), 0)
+    # apply a Character Segmentation and return the output Image
+    word_img= character_seg(rect)
+    return word_img
 
 
 def character_seg(img):
+    #conver the input image int gray scale
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     # Threshold the image
     thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
@@ -48,5 +59,5 @@ def character_seg(img):
     # Iterate through each contour and extract the bounding box
     for contour in contours:
         (x, y, w, h) = cv2.boundingRect(contour)
-        cv2.rectangle(img, (x, y), (x + w, y + h), (100, 100, 255), 2)
+        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0 ), 2)
     return  img
--- pre_vert_hori.py
+++ pre_vert_hori.py
@@ -30,3 +30,32 @@ def sana():
     cv2.imshow('thresh', thresh)
     cv2.imshow('result', result)
     cv2.waitKey()
+# import cv2
+#
+# # Load the image
+# img = cv2.imread('pngImgs/t13.png')
+#
+# # Convert to grayscale
+# gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+#
+# # Threshold the image
+# thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+#
+# # Apply morphological erosion to remove small artifacts
+# kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3,3))
+# eroded = cv2.erode(thresh, kernel, iterations=1)
+#
+# # Apply morphological dilation to expand the characters
+# dilated = cv2.dilate(eroded, kernel, iterations=5)
+#
+# # Find contours in the image
+# contours, hierarchy = cv2.findContours(dilated, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+#
+# # Iterate through each contour and extract the bounding box
+# for contour in contours:
+#     (x, y, w, h) = cv2.boundingRect(contour)
+#     cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+#
+# # Display the result
+# cv2.imshow('Result', img)
+# cv2.waitKey(0)
